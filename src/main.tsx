419851a60f907d516df7d7474d908eee01f4cbcb
Amr Alwaeli <amralwaeli9@gmail.com>
2026-06-18 00:57:11 +0800

    Manus

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+  import { OfflineSyncEngine } from './app/services/OfflineSyncEngine';
+
+  // Start the offline sync background process
+  OfflineSyncEngine.start();
 
   createRoot(document.getElementById("root")!).render(<App />);
 
